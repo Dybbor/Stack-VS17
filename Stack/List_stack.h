c81a1343496795dc253727a64648031e4e65d88e
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2019-02-06 18:03:08 +0300

    add some codes

--- Stack/List_stack.h
+++ Stack/List_stack.h
@@ -1,1 +1,15 @@
 #pragma once
+#include <iostream>
+template <class T>
+struct TLink 
+{
+	T val;			// значение
+	TLink *pNext;   // указательна следующий элемент
+};
+template <class T>
+class TStack
+{
+	TLink *pFirst // указатель на первое звено
+		TStack() { pFirst = NULL} // конструктор по умолчанию
+	
+};
